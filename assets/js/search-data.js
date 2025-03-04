7d529d15811b5a51569743cb5604b4755a21f77a
bobobobosu <57658704+bobobobosu@users.noreply.github.com>
2025-03-04 01:23:00 +0000

    Deploying to gh-pages from @ bobobobosu/bobobobosu.github.io@5b2cb1b39df7d18c31bf24bea7c5ca5e7f502c7b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,51 +398,46 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-cfs-ros",
+          title: 'cfs-ros',
+          description: "A Kinodynamic Motion Planner for ROS",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/projects/cfs-ros/";
+            },},{id: "projects-fpga-accelerated-signed-distance-field-sdf",
+          title: 'FPGA-Accelerated Signed Distance Field (SDF)',
+          description: "Accelerating SDF calculations for convex objects using Xilinx FPGA",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/projects/fpga-sdf/";
+            },},{id: "projects-cuda-accelerated-minimum-distance-query",
+          title: 'CUDA-Accelerated Minimum Distance Query',
+          description: "Minimum distance with gradient among massive number of meshes using CUDA",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+              window.location.href = "/projects/min-dist-cuda/";
+            },},{id: "projects-nodistort",
+          title: 'NoDistort',
+          description: "Recovering distorted drawings from touch screens using IMU",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+              window.location.href = "/projects/nodistort/";
+            },},{id: "projects-robot-layout-optimization",
+          title: 'Robot Layout Optimization',
+          description: "Workload-driven warehouse layout optimization",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
+              window.location.href = "/projects/robot-layout-opt/";
+            },},{id: "projects-robotsweater",
+          title: 'RobotSweater',
+          description: "Customizing Textile and Tactile Skins for Interactive Industrial Robots",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
+              window.location.href = "/projects/robot-sweater/";
+            },},{id: "projects-temporal-relation-extraction",
+          title: 'Temporal Relation Extraction',
+          description: "Leverage language models and formal logic to extract temporal relations of events from text",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
+              window.location.href = "/projects/temporal-relation-extraction/";
+            },},{id: "projects-visual-servoing-a-microscope-with-mrac",
+          title: 'Visual Servoing a Microscope with MRAC',
+          description: "Visual Servoing a Microscope with Model Reference Adaptive Control (MRAC)",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/visual-servoing/";
             },},{
         id: 'social-email',
         title: 'email',
